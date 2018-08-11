7b2317f195d4e289ccb5e5882fab04e9ca716833
Mohamed <mm.medany96@gmail.com>
2018-08-12 01:27:42 +0200

    add the images and modify Word and WordAdapter to show the images and not showing any images in PhraseActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,16 +17,16 @@ public class NumbersActivity extends AppCompatActivity {
         //ArrayList<String> words = new ArrayList<>();
         ArrayList<Word> words = new ArrayList<>();
 
-        words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "otiiko"));
-        words.add(new Word("three", "tolookosu"));
-        words.add(new Word("four", "oyyisa"));
-        words.add(new Word("five", "massokka"));
-        words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven", "kenekaku"));
-        words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "wo’e"));
-        words.add(new Word("ten", "na’aacha"));
+        words.add(new Word("one", "lutti",R.drawable.number_one));
+        words.add(new Word("two", "otiiko",R.drawable.number_two));
+        words.add(new Word("three", "tolookosu",R.drawable.number_three));
+        words.add(new Word("four", "oyyisa",R.drawable.number_four));
+        words.add(new Word("five", "massokka",R.drawable.number_five));
+        words.add(new Word("six", "temmokka",R.drawable.number_six));
+        words.add(new Word("seven", "kenekaku",R.drawable.number_seven));
+        words.add(new Word("eight", "kawinta",R.drawable.number_eight));
+        words.add(new Word("nine", "wo’e",R.drawable.number_nine));
+        words.add(new Word("ten", "na’aacha",R.drawable.number_ten));
 
         /* This code is used to create TextView and put the word in it and view it put this technique
            is not good because if we have 1000 word it will be deadly in memory prespective and
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -53,19 +53,37 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the {@link AndroidFlavor} object located at this position in the list
         Word currentWord = getItem(position);
 
+
         // Find the TextView in the list_item.xml layout with the ID version_name
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
-
         // Get the version name from the current AndroidFlavor object and
         // set this text on the name TextView
         defaultTextView.setText(currentWord.getDefaultTranslation());
 
+
         // Find the TextView in the list_item.xml layout with the ID version_number
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
-
         // Get the version number from the current AndroidFlavor object and
         // set this text on the number TextView
         miwokTextView.setText(currentWord.getMiwokTranslation());
+
+
+        // Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        // Check if an image is provided for this word or not
+        if (currentWord.hasImage()) {
+
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+
+        } else {
+
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
